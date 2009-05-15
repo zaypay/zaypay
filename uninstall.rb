64a4a4e03c270f01bde5ddeaaf049181e35b8a93
Bastiaan Peters <bastiaan@zaypay.com>
2009-05-15 17:45:30 +0200

    Uninstall hook not needed

--- uninstall.rb
+++
@@ -1,7 +0,0 @@
-require 'fileutils'
-zaypay_config_target = File.expand_path(File.dirname(__FILE__) + '/../../../config/zaypay.yml')
-
-if File.exists? zaypay_config_target
-  puts "Should we clean up config/zaypay.yml? [y/N]"
-  FileUtils.rm zaypay_config_target if gets.downcase['y']
-end
